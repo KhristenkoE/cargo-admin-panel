2703d28d074f69841b5a4dc58906675509ed0dde
KhristenkoE <ekhristenko0@gmail.com>
2022-08-11 13:36:40 +0400

    add global variables to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import './styles/global.css';
+import './styles/variables.css';
 
 ReactDOM.render(
 	<App />,
